32b4cef0121e5226d2a43cee6c3dfbf40af3c5b5
spotdemo4 <me@trev.xyz>
2026-07-06 02:11:51 -0400

    fix(fts): update FTS patch for qualified-name API changes

--- packages/duckdb/extensions/fts.nix
+++ packages/duckdb/extensions/fts.nix
@@ -13,13 +13,13 @@
       --replace-fail "SQLIdentifier::ToString" "KeywordHelper::WriteOptionallyQuoted" \
       --replace-fail "SQLString::ToString" "KeywordHelper::WriteQuoted" \
       --replace-fail ".GetIdentifierName()" "" \
-      --replace-fail "sw_qname.Catalog()" "sw_qname.catalog" \
-      --replace-fail "sw_qname.Schema()" "sw_qname.schema" \
-      --replace-fail "sw_qname.Name()" "sw_qname.name" \
-      --replace-fail "qname.SchemaMutable()" "qname.schema" \
+      --replace-fail $'    return QualifiedName(\n        qname.Catalog(),\n        ClientData::Get(context).catalog_search_path->GetDefaultSchema(\n            qname.Catalog()),\n        qname.Name());' $'    qname.schema =\n        ClientData::Get(context).catalog_search_path->GetDefaultSchema(\n            qname.catalog);' \
       --replace-fail "qname.Catalog()" "qname.catalog" \
       --replace-fail "qname.Schema()" "qname.schema" \
       --replace-fail "qname.Name()" "qname.name" \
+      --replace-fail $'  return Catalog::GetEntry<TableCatalogEntry>(\n             context, qname, OnEntryNotFound::RETURN_NULL) != nullptr;' $'  return Catalog::GetEntry<TableCatalogEntry>(\n             context, qname.catalog, qname.schema, qname.name,\n             OnEntryNotFound::RETURN_NULL) != nullptr;' \
+      --replace-fail "Catalog::GetEntry<TableCatalogEntry>(context, qname)" "Catalog::GetEntry<TableCatalogEntry>(context, qname.catalog, qname.schema, qname.name)" \
+      --replace-fail "Catalog::GetEntry<TableCatalogEntry>(context, sw_qname)" "Catalog::GetEntry<TableCatalogEntry>(context, sw_qname.catalog, sw_qname.schema, sw_qname.name)" \
       --replace-fail "Identifier(GetFTSSchemaName(qname))" "GetFTSSchemaName(qname)" \
       --replace-fail "Identifier(name)" "name" \
       --replace-fail "Identifier(column_name)" "column_name" \
