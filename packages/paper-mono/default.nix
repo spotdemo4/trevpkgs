37cc24e1b63f28d93f354992af3a68ff8eb5fa39
trev <me@trev.xyz>
2026-07-24 12:26:12 -0400

    chore(paper-mono): Track coding-ligatures branch

--- packages/paper-mono/default.nix
+++ packages/paper-mono/default.nix
@@ -1,13 +1,13 @@
 {
   lib,
   stdenvNoCC,
-  fetchzip,
+  fetchFromGitHub,
   installFonts,
   nix-update-script,
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "paper-mono";
-  version = "0.300";
+  version = "0.300-unstable-2026-07-02";
   __structuredAttrs = true;
 
   outputs = [
@@ -15,20 +15,31 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     "webfont"
   ];
 
-  src = fetchzip {
-    url = "https://github.com/paper-design/paper-mono/releases/download/v${finalAttrs.version}/paper-mono-v${finalAttrs.version}.zip";
-    hash = "sha256-coWJoVnVrzap8iG5UV84UVQOSDQzgF+a9G5/pn8nD6A=";
+  src = fetchFromGitHub {
+    owner = "paper-design";
+    repo = "paper-mono";
+    rev = "b14a9274d854ac17b6225aa3456e9119a28926dc";
+    hash = "sha256-fPfGrUBD6tNSEbJZaGDhPJwj/GCX7V6sGNVzb8osMok=";
   };
 
   strictDeps = true;
   nativeBuildInputs = [ installFonts ];
 
-  passthru.updateScript = nix-update-script { };
+  dontBuild = true;
+
+  installPhase = ''
+    runHook preInstall
+    runHook postInstall
+  '';
+
+  passthru.updateScript = nix-update-script {
+    extraArgs = [ "--version=branch=coding-ligatures" ];
+  };
 
   meta = {
     description = "Beautiful monospace font for design and code by Paper";
     homepage = "https://github.com/paper-design/paper-mono";
-    changelog = "https://github.com/paper-design/paper-mono/releases/tag/v${finalAttrs.version}";
+    changelog = "https://github.com/paper-design/paper-mono/commits/coding-ligatures";
     license = lib.licenses.ofl;
     maintainers = with lib.maintainers; [ HeitorAugustoLN ];
     platforms = lib.platforms.all;
