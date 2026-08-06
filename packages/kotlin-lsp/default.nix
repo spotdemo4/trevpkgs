a4ff4e3769f1c4844ba3b4e3bcbe2f9b11ae0777
trev <me@trev.xyz>
2026-08-06 12:04:22 -0400

    feat(kotlin-lsp): add aarch64-darwin support

--- packages/kotlin-lsp/default.nix
+++ packages/kotlin-lsp/default.nix
@@ -24,17 +24,23 @@ stdenvNoCC.mkDerivation (finalAttrs: {
           url = "https://download-cdn.jetbrains.com/language-server/kotlin-server/${finalAttrs.version}/kotlin-server-${finalAttrs.version}-aarch64.tar.gz";
           hash = "sha256-769vjedw4TzXPak1U/ls69sIiyow3057VGAADBCXtsU=";
         };
+        aarch64-darwin = fetchzip {
+          url = "https://download-cdn.jetbrains.com/language-server/kotlin-server/${finalAttrs.version}/kotlin-server-${finalAttrs.version}-aarch64.sit";
+          hash = "sha256-qDS5nfZtxAaZUGlaxbcdP8nC1vxYYg1ynj+kwSwo37Q=";
+        };
       };
     in
     sources.${stdenv.hostPlatform.system}
       or (throw "unsupported system: ${stdenv.hostPlatform.system}");
 
   nativeBuildInputs = [
-    autoPatchelfHook
     makeWrapper
+  ]
+  ++ lib.optionals stdenv.hostPlatform.isLinux [
+    autoPatchelfHook
   ];
 
-  buildInputs = [
+  buildInputs = lib.optionals stdenv.hostPlatform.isLinux [
     jdk25
     stdenv.cc.cc.lib
   ];
@@ -44,14 +50,20 @@ stdenvNoCC.mkDerivation (finalAttrs: {
 
     mkdir -p $out/bin $out/share/kotlin-lsp
     cp -r bin build.txt kotlin-lsp.sh lib license modules plugins product-info.json $out/share/kotlin-lsp
+  ''
+  + lib.optionalString stdenv.hostPlatform.isLinux ''
     ln -s ${jdk25}/lib/openjdk $out/share/kotlin-lsp/jbr
-
+  ''
+  + lib.optionalString stdenv.hostPlatform.isDarwin ''
+    cp -r jbr $out/share/kotlin-lsp/
+  ''
+  + ''
     makeWrapper $out/share/kotlin-lsp/bin/intellij-server $out/bin/kotlin-lsp
 
     runHook postInstall
   '';
 
-  doInstallCheck = true;
+  doInstallCheck = stdenv.buildPlatform.canExecute stdenv.hostPlatform;
   installCheckPhase = ''
     runHook preInstallCheck
 
@@ -87,6 +99,7 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     platforms = [
       "x86_64-linux"
       "aarch64-linux"
+      "aarch64-darwin"
     ];
     sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
   };
