e9372961e4983f29448c3834adb9954324c0c6b9
trev <me@trev.xyz>
2026-09-10 11:30:42 -0400

    fix: enable submodules

--- packages/duckdb/extensions/sqlite-scanner.nix
+++ packages/duckdb/extensions/sqlite-scanner.nix
@@ -5,5 +5,6 @@
   repo = "duckdb-sqlite";
   branch = "v1.5-variegata";
   rev = "5274128259f73166c1f37f01190a4601f84c5525";
-  hash = "sha256-ZRFwFIp2v2auDpb820twKXE1x/UKXiZN2EH3Q3C7U20=";
+  hash = "sha256-1HicYGYwf1g7CCOITP0B4dl/whnKMEswOvQZp1TbG8g=";
+  fetchSubmodules = true;
 }
